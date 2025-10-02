d61589f2d24ba6d96f90d721ed81dde7cb78afeb
daria_ppel1 <daria.stsetsevich290@mail.ru>
2025-10-02 15:46:44 +0300

    Add files

--- Lab/Lab2/obj/Debug/net9.0/Lab2.AssemblyInfo.cs
+++ Lab/Lab2/obj/Debug/net9.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd800055ed81b1be7d21b8d4caac762255b4c2a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300496c21b02483a4f978703be7945ec50190f44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
